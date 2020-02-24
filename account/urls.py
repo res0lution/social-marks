5b0a52ddad5e68d6e021bb499ddf0988c2d0b7d4
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-02-24 13:28:50 +0300

    Added social auth via google

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard')
+]
---
+++ account/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import Profile
+from .forms import UserRegistrationForm, ProfileEditForm, UserEditForm
+# Create your views here.
+@login_required
+def dashboard(request):
+    return render(request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
+
+def register(request):
+
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+
+    else:
+        user_form = UserRegistrationForm()
+    
+    return render(
+                request,
+                'account/register.html',
+                {'user_form': user_form}
+            )
+
+@login_required
+def edit(request):
+
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated')
+        else:
+            messages.error(request, 'Error updating your profile')
+
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+                request,
+                'account/edit.html',
+                {
+                    'user_form': user_form,
+                    'profile_form': profile_form
+                }
+            )
+
